273cef2dcdc02a4e3008a74b64ea47910b6be12f
kitamura0102 <raul_almonte001@hotmail.com>
2025-05-06 00:46:27 -0400

    Styling

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
+let htmlText = "";
+let btn = document.getElementById("btn");
+let selectedItem = document.getElementById("select-color");
 
-function colors(){
-    fetch("https://www.thecolorapi.com/scheme?hex=FF0&mode=monochrome-dark&count=4")
-.then(response => response.json())
-.then(function(data){
-    for(let i = 0; i < data.colors.length; i++){
+btn.addEventListener("click", function () {
+  colors();
+});
 
-        console.log(`This is the color I choose ${data.colors[i].hex.value}`)
-        
-    }
-})
+function colors() {
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=FF0&mode=${selectedItem.value}&count=5`
+  )
+    .then((response) => response.json())
+    .then(function (data) {
+      for (let i = 0; i < data.colors.length; i++) {
+        htmlText += `<div class="color-boxes"  style="background-color:${data.colors[i].hex.value}"></div>
+      `;
 
+      }
+      render();
+      htmlText = "";
+    });
 }
 
-colors() 
+
+
+function render() {
+  const container = (document.getElementById("color-palette").innerHTML =
+    htmlText);
+  return container;
+}
+colors();
